35450dfeadd43c296abd3734e2bfd57f86c9527e
its-kayes <kayes.ek8@gmail.com>
2023-07-07 18:34:27 +0600

    implement authorization on review section

--- src/modules/reviews/review.routes.ts
+++ src/modules/reviews/review.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { reviewController } from "./review.controller";
+import { isVerified } from "../../middlewares/isVerified";
+import { Roles } from "../users/user.interface";
 
 const router  = Router();
 
-router.post("/post-review", reviewController.postReview);
+router.post("/post-review", isVerified(Roles.USER),reviewController.postReview);
 
 export { router as reviewRoutes}
